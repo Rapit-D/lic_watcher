859a7d457198edbfd40147301a5aa7efcdde1678
Rapit-D <xingyuan.ding@gmail.com>
2020-05-30 08:53:16 +0800

    first update

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask,render_template
 app = Flask(__name__)
 
 
-@app.route('/<t>')
-def hello_world(t=None):
-    return render_template('base.html', t=t)
+@app.route('/home')
+def hello_world():
+    return render_template('base.html')
