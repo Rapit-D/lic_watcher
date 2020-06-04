4a536255d06bdb8028fd9e9893a71a7b49b6f628
Rapit-D <xingyuan.ding@gmail.com>
2020-06-04 21:11:57 +0800

    commit 06/04

--- app.py
+++ app.py
@@ -1,168 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_moment import Moment
+
 
 app = Flask(__name__)
 
 
-@app.route('/home')
+@app.route('/')
 def hello_world():
     context = [{
         'index': 1,
-        'port': 5800,
+        'port': '5800',
         'server': 'sz-glblic01',
         'description': 'Cadence Server',
         'status': 'UP',
-        'version': '10.16.13'
+        'version': '10.16.13',
+        'country': 'cn'
     },
         {
         'index': 2,
-        'port': 27020,
+        'port': '27020',
         'server': 'sz-glblic01',
         'description': 'Synopls',
         'status': 'UP',
-        'version': '200.1.3'
+        'version': '200.1.3',
+        'country': 'nl'
     }
-        #     {
-        #     'index': 2,
-        #     'port': 27020,
-        #     'server': 'sz-glblic01',
-        #     'description': 'Synopls',
-        #     'status': 'UP',
-        #     'version': '200.1.3'
-        # },
-        #     {
-        #     'index': 2,
-        #     'port': 27020,
-        #     'server': 'sz-glblic01',
-        #     'description': 'Synopls',
-        #     'status': 'UP',
-        #     'version': '200.1.3'
-        # },
-        #     {
-        #     'index': 2,
-        #     'port': 27020,
-        #     'server': 'sz-glblic01',
-        #     'description': 'Synopls',
-        #     'status': 'UP',
-        #     'version': '200.1.3'
-        # },
-        #     {
-        #     'index': 2,
-        #     'port': 27020,
-        #     'server': 'sz-glblic01',
-        #     'description': 'Synopls',
-        #     'status': 'UP',
-        #     'version': '200.1.3'
-        # },
-        #     {
-        #     'index': 2,
-        #     'port': 27020,
-        #     'server': 'sz-glblic01',
-        #     'description': 'Synopls',
-        #     'status': 'UP',
-        #     'version': '200.1.3'
-        # },
-        #     {
-        #     'index': 2,
-        #     'port': 27020,
-        #     'server': 'sz-glblic01',
-        #     'description': 'Synopls',
-        #     'status': 'UP',
-        #     'version': '200.1.3'
-        # },
-        #     {
-        #     'index': 2,
-        #     'port': 27020,
-        #     'server': 'sz-glblic01',
-        #     'description': 'Synopls',
-        #     'status': 'UP',
-        #     'version': '200.1.3'
-        # },
-        #     {
-        #     'index': 2,
-        #     'port': 27020,
-        #     'server': 'sz-glblic01',
-        #     'description': 'Synopls',
-        #     'status': 'UP',
-        #     'version': '200.1.3'
-        # },
-        #     {
-        #     'index': 2,
-        #     'port': 27020,
-        #     'server': 'sz-glblic01',
-        #     'description': 'Synopls',
-        #     'status': 'UP',
-        #     'version': '200.1.3'
-        # },
-        #     {
-        #     'index': 2,
-        #     'port': 27020,
-        #     'server': 'sz-glblic01',
-        #     'description': 'Synopls',
-        #     'status': 'UP',
-        #     'version': '200.1.3'
-        # },
-        #     {
-        #     'index': 2,
-        #     'port': 27020,
-        #     'server': 'sz-glblic01',
-        #     'description': 'Synopls',
-        #     'status': 'UP',
-        #     'version': '200.1.3'
-        # },
-        #     {
-        #     'index': 2,
-        #     'port': 27020,
-        #     'server': 'sz-glblic01',
-        #     'description': 'Synopls',
-        #     'status': 'UP',
-        #     'version': '200.1.3'
-        # },
-        #     {
-        #     'index': 2,
-        #     'port': 27020,
-        #     'server': 'sz-glblic01',
-        #     'description': 'Synopls',
-        #     'status': 'UP',
-        #     'version': '200.1.3'
-        # },
-        #     {
-        #     'index': 2,
-        #     'port': 27020,
-        #     'server': 'sz-glblic01',
-        #     'description': 'Synopls',
-        #     'status': 'UP',
-        #     'version': '200.1.3'
-        # },
-        #     {
-        #     'index': 2,
-        #     'port': 27020,
-        #     'server': 'sz-glblic01',
-        #     'description': 'Synopls',
-        #     'status': 'UP',
-        #     'version': '200.1.3'
-        # },
-        #     {
-        #     'index': 2,
-        #     'port': 27020,
-        #     'server': 'sz-glblic01',
-        #     'description': 'Synopls',
-        #     'status': 'UP',
-        #     'version': '200.1.3'
-        # },
-        #     {
-        #     'index': 2,
-        #     'port': 27020,
-        #     'server': 'sz-glblic01',
-        #     'description': 'Synopls',
-        #     'status': 'UP',
-        #     'version': '200.1.3'
-        # }
     ]
     return render_template('home.html', data=context)
 
 
-@app.route('/watchedlist')
+@app.route('/watchedlist', methods={"POST", "GET"})  # 添加/删除license 服务器
 def setting():
-
+    data = request.get_json
+    print(data)
     return render_template('watchedlist.html')
